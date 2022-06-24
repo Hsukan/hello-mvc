7238139177ae24e7f395ed7e2f2bae18018c05bf
Hsukan <kei01105@naver.com>
2022-06-24 15:29:18 +0900

    0623(수업 + 실습문제(memberDelete))

--- src/main/java/com/kh/mvc/member/controller/MemberDeleteServlet.java
+++ src/main/java/com/kh/mvc/member/controller/MemberDeleteServlet.java
@@ -18,40 +18,39 @@ import com.kh.mvc.member.model.service.MemberService;
 @WebServlet("/member/memberDelete")
 public class MemberDeleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
 	private MemberService memberService = new MemberService();
-    
+
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * 
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		try {
 			// 2. 사용자 입력값 처리
 			HttpSession session = request.getSession();
 			Member loginMember = (Member) session.getAttribute("loginMember");
 			String memberId = loginMember.getMemberId();
 			System.out.println("delete@memberId = " + memberId);
-			
+
 			// 3. 업무 로직
 			// delete from member where member_id = ?;
 			int result = memberService.deleteMember(memberId);
-			
+
 			// 4. 응답
-			// 탈퇴처리 후 세션 만료
-			if(result > 0 ) {
-				
-			if(session != null)
-				session.invalidate();
-			
-			// 홈으로 돌아가기
-			response.sendRedirect(request.getContextPath() + "/");
+			if (result > 0) {
+
+				if (session != null)
+					session.invalidate();
+
+				response.sendRedirect(request.getContextPath() + "/");
 			}
-			
-		} catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 			throw e;
 		}
-		
+
 	}
 
 }
